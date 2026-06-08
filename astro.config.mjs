89d96abb34636866b1aa4d27b50d028fecddad00
Alejo <dasd-123@hotmail.com>
2026-06-08 18:58:40 -0500

    ruta corregida

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dam322.github.io',
-  base: '/notas-belita/',
+  base: '/notas-belita',
 });
