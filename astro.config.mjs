49223b130783fe61eb23e0ee257f1344e4381508
Alejo <dasd-123@hotmail.com>
2026-06-09 22:48:55 -0500

    cambios en la interfaz

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dam322.github.io',
-  base: '/notas-belita',
+    site: 'https://notas-belita.netlify.app'
 });
